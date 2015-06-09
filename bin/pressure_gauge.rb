3c243d8b4bb016c737206d21d9b565b734e9b0b0
georg.rath <georg.rath@impimba-1-login-1.imp.univie.ac.at>
2015-06-09 11:39:55 +0200

    Fix crash_2 and solve user collisions (dirty).

--- bin/pressure_gauge.rb
+++ bin/pressure_gauge.rb
@@ -3,14 +3,14 @@ require 'colorize'
 
 blacklist = [ 'lorca', 'rivera', 'piwi' ]
 
-def generate_load_bar(slots_by_owner, total_slots, overload_threshold)
+def generate_load_bar(slots_by_owner, owner_to_short, total_slots, overload_threshold)
   load_bar = ''
   remaining_slots = total_slots
   slots_by_owner.each do |owner, slots|
-    load_bar += "#{owner[0] * slots}"
+    load_bar += "#{owner_to_short[owner] * slots}"
     remaining_slots -= slots
   end
-  load_bar += '-' * remaining_slots
+  load_bar += '-' * remaining_slots if remaining_slots >= 0
 
   load_bar[overload_threshold..total_slots] = load_bar[overload_threshold..total_slots].red
   load_bar.insert(overload_threshold, '|') if overload_threshold < total_slots
@@ -42,9 +42,23 @@ def get_formatted_load_avg(slots_by_owner, load_avg, overload_threshold)
 
 end
 
+
+def get_short_for_owner(owner_to_short, owner)
+  possibilities = [ owner[0], owner[0].capitalize, '$', '#', '%', '&', '*' ]
+  possibilities.each do |short|
+    if not owner_to_short.values.include?(short) then
+      return short
+    end
+  end
+  exit -1
+end
+
 info = Hash.new
-# we only need qhost -cb here
-qhost_xml = File.open('test/qhost_cb_j.xml')
+owner_to_short = Hash.new
+
+qhost_xml = %x{qhost -cb -xml}
+qstat_xml = %x{qstat -g t -s r -u '*' -xml}
+
 Nokogiri.XML(qhost_xml).xpath("//host[@name != 'global']").each do |host|
   hostname = host.xpath("@name").to_s
 
@@ -71,13 +85,13 @@ Nokogiri.XML(qhost_xml).xpath("//host[@name != 'global']").each do |host|
 end
 
 # get the juicy job details
-qstat_xml = File.open('test/qstat_gt_sr.xml')
 Nokogiri.XML(qstat_xml).xpath("//job_list").each do |job|
   hostname = job.xpath("queue_name/text()").to_s.split('@').last
   owner = job.xpath("JB_owner/text()").text
   slots = job.xpath("slots/text()").text.to_i
 
-  info[hostname][:jobs][owner] += slots
+  info[hostname][:jobs][owner] += slots unless info[hostname].nil?
+  owner_to_short[owner] = get_short_for_owner(owner_to_short, owner) if owner_to_short[owner].nil? 
 end
 
 # natural sort by: http://stackoverflow.com/questions/4078906/is-there-a-natural-sort-by-method-for-ruby
@@ -86,13 +100,18 @@ info.
     select { |key| key.to_s.match(/compute-.*/) }.
     sort_by {|e| e.first.split(/(\d+)/).map {|a| a =~ /\d+/ ? a.to_i : a }}.
     each.with_index(1) do | (hostname, data), index|
-  shortname = hostname.split('.').first.gsub(/compute/, 'c')
-
-  load_bar = generate_load_bar(data[:jobs], data[:slots], data[:cores])
-  out += sprintf("%-6s: [%s] %20s  %2.0f/%2.0f\t", shortname, load_bar, get_formatted_load_avg(data[:jobs], data[:load_avg], data[:cores]), data[:mem_used], data[:mem_total])
+  shortname = hostname.split('.').first #.gsub(/compute/, 'c')
+  
+  load_bar = generate_load_bar(data[:jobs], owner_to_short, data[:slots], data[:cores])
+  out += sprintf("%-12s: [%s] %20s  %2.0f/%2.0f\t", shortname, load_bar, get_formatted_load_avg(data[:jobs], data[:load_avg], data[:cores]), data[:mem_used], data[:mem_total])
   if index % 1 == 0 then
     puts out
     out = ""
   end
 end
 puts out
+
+puts "Users:"
+owner_to_short.each do |user, short|
+  puts "#{user}: #{short}"
+end
